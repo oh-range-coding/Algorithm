4971549a36d44f86aa1a45982bd361b8c107cfb2
오승환 <99662513+ohsine@users.noreply.github.com>
2022-02-19 21:46:45 +0900

    Add files via upload

---
+++ 1_Greedy/Test/Q3/Oh.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string input;
+	cin >> input;
+
+	int count0 = 0;
+	int count1 = 0;
+	for (int i = 0; i < input.length() - 1; i++)
+	{
+		if (input[i] != input[i + 1])
+		{
+			if (input[i + 1] == '0')
+				count0++;
+			else
+				count1++;
+		}
+	}
+	int result = (count0 < count1 ? count0 : count1);
+	cout << result;
+	return 0;
+}
